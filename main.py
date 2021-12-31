3a3f0a455b614448b2a12e93947d8a509024cf35
Eric Dharmasiri <eric.dharmasiri@gmail.com>
2022-01-01 00:27:43 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from rock_paper_scissors import *
+current_winCount = 0
+current_lossCount = 0
+current_tieCount = 0
 
-showdown()
+recent_win, recent_loss, recent_tie = showdown()
+
+#Send to GUI
+current_winCount = current_winCount + recent_win
+current_lossCount = current_lossCount + recent_loss
+current_tieCount = current_tieCount + recent_tie
